777df766c6eefe885c09f4339880f4744e71a7e1
Salvador Parajon <rcserna09@gmail.com>
2024-02-29 13:51:55 -0800

    Player Control
    Added another car/player to the project to play as a two player game.

--- P5PlayerControlSerna/Assets/Scripts/PlayerController.cs
+++ P5PlayerControlSerna/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     private float speed = 20.0f;
     private float turnSpeed = 99.0f;
     private float horizontalInput;
     private float forwardInput;
+    public string inputID;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +22,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Axis setup
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
+
+        //Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //Rotate the vehicle left and right
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
 
         // Move the vehicle forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
